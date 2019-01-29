049201dfb6ee27e7a948d92e44ffc2b501a070ed
sbhangal <sbhangal3@gmail.com>
2019-01-28 22:48:23 -0800

    Updating profile.

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function jobDescToggle(id) {
+function jobDescToggle_preserve_space(id) {
     let x = document.getElementById(id);
     if (x.style.visibility === "visible") {
         x.style.visibility = "hidden";
     } else {
         x.style.visibility = "visible";
+    }
+}
+
+function jobDescToggle(id) {
+    let x = document.getElementById(id);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
